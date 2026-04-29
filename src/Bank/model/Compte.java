0799a0c67fb6e566e4fccea8126f483b022f68a9
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-04-30 01:09:21 +0200

    Ajout des méthodes pour gérer les transactions et consultation des données dans la classe Compte

--- src/Bank/model/Compte.java
+++ src/Bank/model/Compte.java
@@ -1,4 +1,38 @@
 package Bank.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Compte {
+    private final String iban;
+    private double solde;
+    private final List<Transaction> transactions;
+
+    // COnstructeur d'un compte bancaire
+    public Compte(String iban, double solde) {
+        this.iban = iban;
+        this.solde = solde;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void ajouterTransaction(Transaction transaction) {
+        transactions.add(transaction);
+        solde += transaction.getMontant();
+    }
+    public void retirerTransaction(Transaction transaction) {
+        transactions.remove(transaction);
+        solde -= transaction.getMontant();
+    }
+
+    //Getter
+    public String getIban() {
+        return iban;
+    }
+    public double getSolde() {
+        return solde;
+    }
+    public List<Transaction> getTransactions() {
+        return Collections.unmodifiableList(transactions);
+    }
 }
