59a98a4325df128e6b4759a16e5b9f4253b64e22
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-05-04 18:46:18 +0200

    Création de la classe `StubCompteViewModel` dans le package `Bank.viewModel.stubViewModel` avec implémentation des propriétés définies dans l'interface `CompteViewModel`.

---
+++ src/Bank/viewModel/stubViewModel/StubCompteViewModel.java
@@ -0,0 +1,22 @@
+package Bank.viewModel.stubViewModel;
+
+import Bank.viewModel.CompteViewModel;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class StubCompteViewModel implements CompteViewModel {
+    private final StringProperty iban = new SimpleStringProperty("");
+    private final DoubleProperty solde = new SimpleDoubleProperty(0.0);
+
+    /*!< Implementation des méthodes définit dans l'interface 'CompteViewModel'. */
+    @Override
+    public StringProperty ibanProperty() {
+        return iban;
+    }
+    @Override
+    public DoubleProperty soldeProperty() {
+        return solde;
+    }
+}
