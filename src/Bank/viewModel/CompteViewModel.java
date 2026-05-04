c377decbd8fade9f56d3b4c8aa888581897270cd
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-05-04 18:45:50 +0200

    Création de la classe `StubClientDetailViewModel` et de l'interface `CompteViewModel` dans le package `Bank.viewModel` avec les propriétés nécessaires pour la gestion des détails et comptes clients.

---
+++ src/Bank/viewModel/CompteViewModel.java
@@ -0,0 +1,11 @@
+package Bank.viewModel;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.StringProperty;
+
+public interface CompteViewModel {
+    // l'iban  du compte
+    StringProperty ibanProperty();
+    //Le solde du compte
+    DoubleProperty soldeProperty();
+}
---
+++ src/Bank/viewModel/stubViewModel/StubClientDetailViewModel.java
@@ -0,0 +1,51 @@
+package Bank.viewModel.stubViewModel;
+
+import Bank.model.*;
+import Bank.viewModel.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart;
+
+public class StubClientDetailViewModel implements ClientDetailsViewModel {
+    private final StringProperty nomComplet = new SimpleStringProperty("");
+    private final DoubleProperty soldeTotal = new SimpleDoubleProperty(0.0);
+    private final ObservableList<CompteViewModel> comptes = FXCollections.observableArrayList();
+    private final ObservableList<TransactionViewModel> transactions = FXCollections.observableArrayList();
+    private final ObservableList<PieChart.Data> statBudget = FXCollections.observableArrayList();
+
+    // Methode d'injection d'un client et de mise a jour de son nomComplet
+    public void updateFromClient(Client client){
+        if (client != null) {
+            nomComplet.set(client.getNomComplet());
+            soldeTotal.set(client.getSoldeTotal());
+        }
+    }
+    @Override
+    public StringProperty nomCompletProperty() {
+        return nomComplet;
+    }
+    @Override
+    public DoubleProperty soldeTotalProperty(){
+        return soldeTotal;
+    }
+    @Override
+    public ObservableList<CompteViewModel> getComptesOfClient() {
+        return comptes;
+    }
+    @Override
+    public ObservableList<TransactionViewModel> getTransactionsOfCompte() {
+        return transactions;
+    }
+    @Override
+    public ObservableList<PieChart.Data> getStaticClientBudget() {
+        return statBudget;
+    }
+    @Override
+    public void setCompteActif(Compte compte) {
+        // Aucune implementation dans ce fichier
+    }
+}
