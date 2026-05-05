f292e4c14a5c06e38bfb934b14a96040d2c8ae1c
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-05-05 21:41:30 +0200

    Ajout de l'attribution automatique d'un compte courant lors de l'ajout d'un client et des fonctionnalités de tri des clients dans `StubBanqueViewModel`.

--- src/Bank/viewModel/stubViewModel/StubBanqueViewModel.java
+++ src/Bank/viewModel/stubViewModel/StubBanqueViewModel.java
@@ -1,7 +1,10 @@
 package Bank.viewModel.stubViewModel;
 
 import Bank.model.Client;
+import Bank.model.Compte;
+import Bank.model.TypeCompte;
 import Bank.viewModel.BanqueViewModel;
+import Bank.viewModel.CompteViewModel;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -16,6 +19,9 @@ import java.time.LocalDate;
 public class StubBanqueViewModel implements BanqueViewModel {
     private final ObservableList<Client> clients = FXCollections.observableArrayList();
     private final StringProperty filtre = new SimpleStringProperty("");
+    private int dernierId = 0;
+    private String dernierTri = "";
+    private boolean ascendant = true;
 
     @Override
     public ObservableList<Client> getClientsTrier() {
@@ -27,6 +33,48 @@ public class StubBanqueViewModel implements BanqueViewModel {
     }
     @Override
     public void ajouterClientSimple(Client client) {
-        clients.add(client);
+        dernierId++;
+        // Toutes les informations proviennent de la popUp
+        Client clientFinalId = new Client(dernierId, client.getNom(), client.getPrenom(), client.getDateNaissance(),
+                client.getAdresse(), client.getEmail(), client.getTelephone()
+        );
+
+        // Logique d'attribution de compte courant par defaut
+
+        Compte compteCourant = new Compte(TypeCompte.Courant, 50.0);
+        clientFinalId.ajouterCompte(compteCourant);
+        clients.add(clientFinalId);
     }
+
+    @Override
+    public void trierClients(String trierPar) {
+        if (trierPar.equals(dernierTri)){
+            ascendant = !ascendant;
+        }else{
+            ascendant = true;
+            dernierTri = trierPar;
+        }
+        switch (trierPar.toLowerCase()) {
+            case "id":
+                clients.sort((c1, c2) -> ascendant ?
+                        Integer.compare(c1.getId(), c2.getId()) :
+                        Integer.compare(c2.getId(), c1.getId())
+                        );
+                break;
+            case "nom":
+                // On trie par nom et si le nom est identique par prénom
+                clients.sort((c1, c2) -> {
+                    int res = c1.getNom().compareToIgnoreCase(c2.getNom());
+                    if (res == 0) return c1.getPrenom().compareToIgnoreCase(c2.getPrenom());
+                    return ascendant ? res : -res;
+                });
+                break;
+        }
+    }
+
+    @Override
+    public boolean isAscendant(){
+        return ascendant;
+    }
+
 }
