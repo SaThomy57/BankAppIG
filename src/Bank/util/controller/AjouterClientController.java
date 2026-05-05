3d9cc7e4e9d24204a6dd97faff97b91b6e1b446f
SaThomy57 <148277208+SaThomy57@users.noreply.github.com>
2026-05-05 18:09:52 +0200

    Ajout d'une alerte dans `AjouterClientController` pour signaler les champs obligatoires non remplis lors de l'enregistrement d'un client.

--- src/Bank/util/controller/AjouterClientController.java
+++ src/Bank/util/controller/AjouterClientController.java
@@ -2,6 +2,7 @@ package Bank.util.controller;
 
 import Bank.model.Client;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -18,6 +19,20 @@ public class AjouterClientController {
 
     @FXML
     private void handleEnregistrer(){
+        // On récupere les valeurs et on supprime les espaces avec trim()
+        String nom = nomField.getText().trim();
+        String prenom = prenomField.getText().trim();
+
+        //Requiered : conditions d'obligation
+        if (nom.isEmpty() || prenom.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Champs obligatoires");
+            alert.setHeaderText(null);
+            alert.setContentText("Veuillez remplir tous les champs obligatoires !");
+            alert.showAndWait();
+            return;
+        }
+
         if (nomField != null && prenomField != null && dateNaissancePicker != null) {
             nouveauClient = new Client(0, nomField.getText(), prenomField.getText(), dateNaissancePicker.getValue());
         }
